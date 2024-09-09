f6055ada2405f485bb56f7aa8f2798bada404c7b
Martin <martinpellizzer@gmail.com>
2024-09-09 12:25:48 +0200

    '

--- magazine-layout/main.py
+++ magazine-layout/main.py
@@ -6,10 +6,12 @@ fonts_folderpath = f'{vault_folderpath}/fonts'
 a4_w = 2480
 a4_h = 3508
 
-ml = 300
-mt = 500
-mr = 500
-mb = 800
+margin_mul = 0.75
+
+ml = 300*margin_mul
+mt = 500*margin_mul
+mr = 500*margin_mul
+mb = 800*margin_mul
 
 text_area_x1 = ml
 text_area_y1 = mt
@@ -47,6 +49,11 @@ if 0:
         draw.line((text_area_x1, text_area_y1 + row_h*i, text_area_x2, text_area_y1 + row_h*i), fill='#00ffff', width=4)
         draw.line((text_area_x1, text_area_y1 + row_h*i - line_h, text_area_x2, text_area_y1 + row_h*i - line_h), fill='#00ffff', width=4)
 
+    draw.line((text_area_x1, text_area_y1, text_area_x2, text_area_y1), fill='#ff0000', width=4)
+    draw.line((text_area_x1, text_area_y2, text_area_x2, text_area_y2), fill='#ff0000', width=4)
+    draw.line((text_area_x1, text_area_y1, text_area_x1, text_area_y2), fill='#ff0000', width=4)
+    draw.line((text_area_x2, text_area_y1, text_area_x2, text_area_y2), fill='#ff0000', width=4)
+
 with open('body-text.txt', encoding='utf-8') as f: body_text = f.read()
 body_text = body_text.strip().replace('\n', ' ').replace('  ', ' ')
 
@@ -66,21 +73,42 @@ line_x = text_area_x1
 line_y = text_area_y1
 i = 0
 for line in lines:
-    draw.text((line_x, line_y + font_size*i*line_spacing), line, '#000000', font=font)
+    # draw.text((line_x - line_h*2, line_y + font_size*i*line_spacing), str(i%line_num+1), '#000000', font=font)
+    
+    ## align-justify
+    word_x = text_area_x1
+    _, _, line_w, _ = font.getbbox(line)
+    space_left = col_w - line_h - line_w
+    words = line.split(' ')
+    word_num = len(words)
+    space_add = space_left / (word_num-1)
+    for word in words:
+        draw.text((word_x, line_y + font_size*i*line_spacing), word, '#000000', font=font)
+        _, _, word_w, _ = font.getbbox(word)
+        _, _, space_w, _ = font.getbbox(' ')
+        word_x += word_w + space_w + space_add
+
     i += 1
 
 line_x = text_area_x1 + col_w
 line_y = text_area_y1
 i = 0
 for line in lines:
-    draw.text((line_x, line_y + font_size*i*line_spacing), line, '#000000', font=font)
+    ## align-justify
+    word_x = text_area_x1 + col_w
+    _, _, line_w, _ = font.getbbox(line)
+    space_left = col_w - line_h - line_w
+    words = line.split(' ')
+    word_num = len(words)
+    space_add = space_left / (word_num-1)
+    for word in words:
+        draw.text((word_x, line_y + font_size*i*line_spacing), word, '#000000', font=font)
+        _, _, word_w, _ = font.getbbox(word)
+        _, _, space_w, _ = font.getbbox(' ')
+        word_x += word_w + space_w + space_add
+        
     i += 1
 
-
-draw.line((text_area_x1, text_area_y1, text_area_x2, text_area_y1), fill='#ff0000', width=4)
-draw.line((text_area_x1, text_area_y2, text_area_x2, text_area_y2), fill='#ff0000', width=4)
-draw.line((text_area_x1, text_area_y1, text_area_x1, text_area_y2), fill='#ff0000', width=4)
-draw.line((text_area_x2, text_area_y1, text_area_x2, text_area_y2), fill='#ff0000', width=4)
 
 img.save('output.jpg')
 img.show()
